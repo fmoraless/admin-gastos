e036e80f9d9d2da66dcebeda8243c37d258da108
fmoraless <franticzero@gmail.com>
2023-07-16 18:50:22 -0400

    init commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <div>
+    <header>
+      <h1>Planificador de gastos</h1>
+    </header>
+  </div>
+</template>
+
+<style></style>
---
+++ src/components/Filtro.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <div>
+    <header>
+      <h2>Filtro de gastos</h2>
+    </header>
+  </div>
+</template>
+
+<style scoped></style>
