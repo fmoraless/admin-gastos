03a55385f3c4a238520af8066ad74d97ae660742
fmoraless <franticzero@gmail.com>
2023-07-19 00:10:38 -0400

    feat(budget-form): create budget form

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
-<script setup></script>
+<script setup>
+import Presupuesto from './components/Presupuesto.vue'
+</script>
 
 <template>
   <div>
     <header>
       <h1>Planificador de gastos</h1>
+      <div class="contenedor-header contenedor sombra">
+        <Presupuesto />
+      </div>
     </header>
   </div>
 </template>
@@ -11,8 +16,8 @@
 <style>
 :root {
   --azul: #3b82f6;
-  --blanco: #FFF;
-  --gris-claro: #F5F5F5;
+  --blanco: #fff;
+  --gris-claro: #f5f5f5;
   --gris: #94a3b8;
   --gris-oscuro: #64748b;
   --negro: #000;
@@ -28,7 +33,7 @@ html {
 }
 body {
   font-size: 1.6rem;
-  font-family: "Lato", sans-serif;
+  font-family: 'Lato', sans-serif;
   background-color: var(--gris-claro);
 }
 h1 {
@@ -45,5 +50,21 @@ header h1 {
   margin: 0;
   color: var(--blanco);
   text-align: center;
+}
+.contenedor {
+  width: 90%;
+  max-width: 80rem;
+  margin: 0 auto;
+}
+.contenedor-header {
+  margin-top: -5rem;
+  transform: translateY(5rem);
+  padding: 5rem;
+}
+.sombra {
+  box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);
+  background-color: var(--blanco);
+  border-radius: 1.2rem;
+  padding: 5rem;
 }
 </style>
