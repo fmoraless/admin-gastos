5570d5a54cd485248ac5deae9f0b04d3f68a6d80
fmoraless <franticzero@gmail.com>
2023-07-22 12:46:50 -0400

    feat(budget-form): add budget modal

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 import Presupuesto from './components/ThePresupuesto.vue'
 import ControlPresupuesto from '@/components/ControlPresupuesto.vue'
 import iconoNuevoGasto from './assets/img/nuevo-gasto.svg'
+import Modal from './components/TheModal.vue'
 
+const modal = reactive({
+  mostrar: false,
+  animar: false
+})
 const presupuesto = ref(0)
 const disponible = ref(0)
 
 const definirPresupuesto = (cantidad) => {
   presupuesto.value = cantidad
   disponible.value = cantidad
+}
+
+const mostrarModal = () => {
+  modal.mostrar = true
+  modal.animar = true
+}
+const ocultarModal = () => {
+  modal.mostrar = false
+  modal.animar = false
 }
 </script>
 
@@ -24,8 +38,10 @@ const definirPresupuesto = (cantidad) => {
     </header>
     <main v-if="presupuesto > 0">
       <div class="crear-gasto">
-        <img :src="iconoNuevoGasto" alt="icono nuevo gasto" />
+        <img :src="iconoNuevoGasto" alt="icono nuevo gasto" @click="mostrarModal" />
       </div>
+
+      <Modal v-if="modal.mostrar" @ocultar-modal="ocultarModal" />
     </main>
   </div>
 </template>
