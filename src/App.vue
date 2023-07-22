0d1635e9794b49dea153ebf242792f31144a2f98
fmoraless <franticzero@gmail.com>
2023-07-22 13:09:12 -0400

    feat(budget-form): animate budget modal

--- src/App.vue
+++ src/App.vue
@@ -19,11 +19,15 @@ const definirPresupuesto = (cantidad) => {
 
 const mostrarModal = () => {
   modal.mostrar = true
-  modal.animar = true
+  setTimeout(() => {
+    modal.animar = true
+  }, 300)
 }
 const ocultarModal = () => {
-  modal.mostrar = false
   modal.animar = false
+  setTimeout(() => {
+    modal.mostrar = false
+  }, 300)
 }
 </script>
 
@@ -41,7 +45,7 @@ const ocultarModal = () => {
         <img :src="iconoNuevoGasto" alt="icono nuevo gasto" @click="mostrarModal" />
       </div>
 
-      <Modal v-if="modal.mostrar" @ocultar-modal="ocultarModal" />
+      <Modal v-if="modal.mostrar" @ocultar-modal="ocultarModal" :modal="modal" />
     </main>
   </div>
 </template>
