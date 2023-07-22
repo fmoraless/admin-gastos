d3da1dd8b7b17ef3702f6a4772ade9a85dde2c67
fmoraless <franticzero@gmail.com>
2023-07-22 02:23:46 -0400

    feat(budget-form): add budget panel

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 import Presupuesto from './components/ThePresupuesto.vue'
 import ControlPresupuesto from '@/components/ControlPresupuesto.vue'
+import iconoNuevoGasto from './assets/img/nuevo-gasto.svg'
 
 const presupuesto = ref(0)
 const disponible = ref(0)
@@ -21,6 +22,11 @@ const definirPresupuesto = (cantidad) => {
         <control-presupuesto v-else :presupuesto="presupuesto" :disponible="disponible" />
       </div>
     </header>
+    <main v-if="presupuesto > 0">
+      <div class="crear-gasto">
+        <img :src="iconoNuevoGasto" alt="icono nuevo gasto" />
+      </div>
+    </main>
   </div>
 </template>
 
@@ -77,5 +83,14 @@ header h1 {
   background-color: var(--blanco);
   border-radius: 1.2rem;
   padding: 5rem;
+}
+.crear-gasto {
+  position: fixed;
+  bottom: 5rem;
+  right: 5rem;
+}
+.crear-gasto img {
+  width: 5rem;
+  cursor: pointer;
 }
 </style>
