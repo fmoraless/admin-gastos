400a3a0bb51c294f3e7089cb5508d9047274315f
fmoraless <franticzero@gmail.com>
2023-07-29 11:20:07 -0400

    feat(bills-list): add bill component

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@ import Presupuesto from './components/ThePresupuesto.vue'
 import ControlPresupuesto from '@/components/ControlPresupuesto.vue'
 import iconoNuevoGasto from './assets/img/nuevo-gasto.svg'
 import Modal from './components/TheModal.vue'
+import Gasto from './components/TheGasto.vue'
+import { generarId } from './helpers'
 
 const modal = reactive({
   mostrar: false,
@@ -42,7 +44,15 @@ const ocultarModal = () => {
 const guardarGasto = () => {
   gastos.value.push({
     ...gasto,
-    id: 123
+    id: generarId()
+  })
+  ocultarModal()
+  Object.assign(gasto, {
+    id: null,
+    nombre: '',
+    cantidad: 0,
+    categoria: '',
+    fecha: Date.now()
   })
 }
 </script>
@@ -57,6 +67,12 @@ const guardarGasto = () => {
       </div>
     </header>
     <main v-if="presupuesto > 0">
+      <div class="listado_gastos contenedor">
+        <h2>{{ gastos.length > 0 ? 'Gastos' : 'No hay gastos' }}</h2>
+        <Gasto v-for="gasto in gastos" :key="gasto.id" :gasto="gasto">
+          {{ gasto }}
+        </Gasto>
+      </div>
       <div class="crear-gasto">
         <img :src="iconoNuevoGasto" alt="icono nuevo gasto" @click="mostrarModal" />
       </div>
@@ -136,5 +152,13 @@ header h1 {
 .crear-gasto img {
   width: 5rem;
   cursor: pointer;
+}
+.listado_gastos {
+  margin-top: 10rem;
+}
+
+listado_gastos h2 {
+  font-weight: 900;
+  color: var(--gris-oscuro);
 }
 </style>
