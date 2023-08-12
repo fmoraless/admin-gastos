f5dbb5d7eb052e3494cb112a8bb6f027cdcf2733
fmoraless <franticzero@gmail.com>
2023-08-12 13:21:01 -0400

    feat(budget-form): show budget percent

--- src/components/ControlPresupuesto.vue
+++ src/components/ControlPresupuesto.vue
@@ -1,6 +1,10 @@
 <script setup>
-import imagen from '../assets/img/grafico.jpg'
+import { computed } from 'vue'
+import CircleProgress from 'vue3-circle-progress'
+import 'vue3-circle-progress/dist/circle-progress.css'
 import { formatearCantidad } from '@/helpers'
+
+defineEmits(['reset-app'])
 
 const props = defineProps({
   presupuesto: {
@@ -16,13 +20,24 @@ const props = defineProps({
     required: true
   }
 })
-defineEmits(['reset-app'])
+
+const porcentaje = computed(() => {
+  return parseInt(((props.presupuesto - props.disponible) / props.presupuesto) * 100)
+})
 </script>
 
 <template>
   <div class="dos-columnas">
     <div class="contenedor-grafico">
-      <img :src="imagen" alt="img" />
+      <p class="porcentaje">{{ porcentaje }}%</p>
+      <CircleProgress
+        :percent="porcentaje"
+        :size="250"
+        :borderWidth="30"
+        :border-bg-width="30"
+        fill-color="#3b82f6"
+        empty-color="#f5F5F5"
+      />
     </div>
     <div class="contenedor-presupuesto">
       <button type="button" @click="$emit('reset-app')" class="reset-app">Resetear App</button>
@@ -43,6 +58,21 @@ defineEmits(['reset-app'])
 </template>
 
 <style scoped>
+.contenedor-grafico {
+  position: relative;
+}
+.porcentaje {
+  position: absolute;
+  margin: auto;
+  top: calc(50% - 1.5rem);
+  left: 0;
+  right: 0;
+  text-align: center;
+  z-index: 100;
+  font-size: 3rem;
+  font-weight: 900;
+  color: var(--gris-oscuro);
+}
 .dos-columnas {
   display: flex;
   flex-direction: column;
